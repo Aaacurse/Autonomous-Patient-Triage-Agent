8c94f5558d552598982a071b323105b60636ea32
Aaacurse <rajputaakarsh2004@gmail.com>
2026-07-07 01:50:43 +0530

    Added create user function

--- backend/app/db/crud.py
+++ backend/app/db/crud.py
@@ -2,11 +2,22 @@ from app.db.models import User
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from pydantic import EmailStr
+from app.core.security import hash_password
 
 
 async def get_user_by_email(db:AsyncSession,email:EmailStr):
     result=await db.execute(select(User).where(User.email==email))
     
     user=result.scalar_one_or_none()
+    
+    return user
+
+
+async def create_user(db:AsyncSession,email:EmailStr,full_name:str,password:str):
+    hashed_password=hash_password(password)
+    user=User(email=email,full_name=full_name,hashed_password=hashed_password)
+    db.add(user) 
+    await db.commit()
+    await db.refresh(user)
     
     return user
